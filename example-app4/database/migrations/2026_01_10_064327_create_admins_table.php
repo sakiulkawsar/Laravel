ab19eb0c227e9f9a050b00f24f67ca170961b5fd
sakiulkawsar <mr.sakiulkawsar@gmail.com>
2026-01-17 23:18:34 +0600

    make hospital

--- example-app4/database/migrations/2026_01_10_064327_create_admins_table.php
+++ example-app4/database/migrations/2026_01_10_064327_create_admins_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->id();
-           
-        $table->string('name');
-        $table->string('email')->unique();
-        $table->string('password');
-        $table->rememberToken();
-       
+              $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
